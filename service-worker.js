92ac1243214c5a59151f7be749be7f8390f628fd
Rabie Mohamed <rabiee3.mohamed@gmail.com>
2025-10-22 04:02:40 +0300

    adding service worker files

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'sledkom';
+const OFFLINE_URLS = [
+  './',
+  './index.html',
+  './resources/', // cache all JS/CSS/images inside resources if needed
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(OFFLINE_URLS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('./index.html'))
+  );
+});
